c8ebc59ce0c42a333284b6a326cc1723a51d23b2
차준호 <ckwnsgh4@naver.com>
2026-05-08 15:55:17 +0900

    initial commit

---
+++ src/main/java/org/study/inhamatch/InhaMatchApplication.java
@@ -0,0 +1,13 @@
+package org.study.inhamatch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InhaMatchApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(InhaMatchApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/study/inhamatch/InhaMatchApplicationTests.java
@@ -0,0 +1,13 @@
+package org.study.inhamatch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InhaMatchApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
