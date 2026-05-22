90d6155389555e6ae8df7b3084f3d1cf17e53f80
지원 <cjwcgz@gmail.com>
2026-05-22 15:25:10 +0900

    feat : auth 기능 구현

---
+++ src/main/java/org/study/inhamatch/domain/auth/service/CustomOAuth2UserService.java
@@ -0,0 +1,35 @@
+package org.study.inhamatch.domain.auth.service;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.oauth2.client.userinfo.DefaultOAuth2UserService;
+import org.springframework.security.oauth2.client.userinfo.OAuth2UserRequest;
+import org.springframework.security.oauth2.client.userinfo.OAuth2UserService;
+import org.springframework.security.oauth2.core.OAuth2AuthenticationException;
+import org.springframework.security.oauth2.core.user.OAuth2User;
+import org.springframework.stereotype.Service;
+import org.study.inhamatch.domain.auth.entity.User;
+import org.study.inhamatch.domain.auth.repository.UserRepository;
+
+import java.util.Map;
+
+@Service
+@RequiredArgsConstructor
+public class CustomOAuth2UserService implements OAuth2UserService<OAuth2UserRequest, OAuth2User> {
+    private final UserRepository userRepository;
+
+    @Override
+    public OAuth2User loadUser(OAuth2UserRequest userRequest) throws OAuth2AuthenticationException {
+        OAuth2UserService<OAuth2UserRequest, OAuth2User> delegate = new DefaultOAuth2UserService();
+        OAuth2User oAuth2User = delegate.loadUser(userRequest);
+
+        Map<String, Object> attributes = oAuth2User.getAttributes();
+        String email = (String) attributes.get("email");
+
+        User user = userRepository.findByEmail(email)
+                .orElseGet(() -> {
+                    String tempStudentId = email.split("@")[0];
+                    return userRepository.save(User.create(email, tempStudentId));
+                });
+        return oAuth2User;
+    }
+}
